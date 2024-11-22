26c92789d40880968bb455059d96dfad211f19e5
ebonian <52095091+ebonian@users.noreply.github.com>
2024-11-23 02:48:54 +0700

    wezterm

--- windows/wezterm/keybinds.lua
+++ windows/wezterm/keybinds.lua
@@ -31,9 +31,9 @@ function module.apply_to_config(config)
         { key = "7",  mods = "LEADER",       action = wezterm.action { ActivateTab = 6 } },
         { key = "8",  mods = "LEADER",       action = wezterm.action { ActivateTab = 7 } },
         { key = "9",  mods = "LEADER",       action = wezterm.action { ActivateTab = 8 } },
-        { key = "&",  mods = "LEADER|SHIFT", action = wezterm.action { CloseCurrentTab = { confirm = false } } },
-        { key = "d",  mods = "LEADER",       action = wezterm.action { CloseCurrentPane = { confirm = false } } },
-        { key = "x",  mods = "LEADER",       action = wezterm.action { CloseCurrentPane = { confirm = false } } },
+        { key = "&",  mods = "LEADER|SHIFT", action = wezterm.action { CloseCurrentTab = { confirm = true } } },
+        { key = "d",  mods = "LEADER",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
+        { key = "x",  mods = "LEADER",       action = wezterm.action { CloseCurrentPane = { confirm = true } } },
     }
 end
 
