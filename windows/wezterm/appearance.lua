637a66f3b5c481db5f8e33d10be53c4b85081d14
ebonian <52095091+ebonian@users.noreply.github.com>
2024-11-23 00:25:14 +0700

    wezterm

--- windows/wezterm/appearance.lua
+++ windows/wezterm/appearance.lua
@@ -5,7 +5,58 @@ local module = {}
 function module.apply_to_config(config)
     config.font = wezterm.font 'CaskaydiaCove Nerd Font'
 
-    config.hide_tab_bar_if_only_one_tab = true
+    config.hide_tab_bar_if_only_one_tab = false
+
+    config.inactive_pane_hsb = {
+        saturation = 0.8,
+        brightness = 0.8,
+    }
+
+    config.colors = {
+        background = '#141414',
+        foreground = "#eeffff",
+
+        ansi = {
+            "#212121",
+            "#f07178",
+            "#c3e88d",
+            "#ffcb6b",
+            "#82aaff",
+            "#c792ea",
+            "#89ddff",
+            "#eeffff"
+        },
+        brights = {
+            "#4a4a4a",
+            "#f07178",
+            "#c3e88d",
+            "#ffcb6b",
+            "#82aaff",
+            "#c792ea",
+            "#89ddff",
+            "#ffffff"
+        },
+
+        cursor_bg = "#eeffff",
+        cursor_border = "#eeffff",
+        cursor_fg = "#212121",
+
+        selection_bg = "#eeffff",
+        selection_fg = "#212121",
+
+        indexed = {
+            [16] = "#f78c6c",
+            [17] = "#ff5370",
+            [18] = "#303030",
+            [19] = "#353535",
+            [20] = "#b2ccd6",
+            [21] = "#eeffff",
+        },
+
+        tab_bar = {
+            background = '#141414',
+        },
+    }
 end
 
 return module
--- windows/wezterm/main.lua
+++ windows/wezterm/main.lua
@@ -1,13 +1,46 @@
+-- wezterm config
 local wezterm = require 'wezterm'
-
-local appearance = require 'appearance'
-local keybinds = require 'keybinds'
 
 local config = wezterm.config_builder()
 
 config.default_prog = { 'C:/Program Files/Git/bin/bash.exe', '-i', '-l' }
+config.window_close_confirmation = 'NeverPrompt'
+
+
+-- config modules
+local appearance = require 'appearance'
+local keybinds = require 'keybinds'
+
+
+-- bar plugin
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(
+    config,
+    {
+        modules = {
+            workspace = {
+                enabled = false
+            },
+            leader = {
+                enabled = false
+            },
+            pane = {
+                enabled = false
+            },
+            clock = {
+                enabled = false,
+            },
+            cwd = {
+                enabled = false,
+            },
+        },
+    }
+)
 
 appearance.apply_to_config(config)
 keybinds.apply_to_config(config)
+
+
+
 
 return config
