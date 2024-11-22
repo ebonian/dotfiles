721ceba6e9358e6325d10513670d36b078fe731f
ebonian <52095091+ebonian@users.noreply.github.com>
2024-11-23 03:02:35 +0700

    wezterm

--- windows/wezterm/program.lua
+++ windows/wezterm/program.lua
@@ -1,9 +1,20 @@
+local wezterm = require 'wezterm'
+
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+    local tab, pane, window = mux.spawn_window {}
+    window:gui_window():maximize()
+end)
+
 local module = {}
 
 function module.apply_to_config(config)
     config.default_prog = { 'C:/Program Files/Git/bin/bash.exe', '-i', '-l' }
 
     config.window_close_confirmation = 'NeverPrompt'
+
+    config.window_decorations = "RESIZE"
 
     config.initial_cols = 140
     config.initial_rows = 40
