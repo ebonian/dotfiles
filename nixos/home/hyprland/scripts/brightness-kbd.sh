44971ad2f69862000ae324f1230b4de124d0b83c
ebonian <52095091+ebonian@users.noreply.github.com>
2024-11-23 11:59:50 +0700

    53 current  2024-11-23 11:59:48  24.05.20241116.e8c38b7  6.6.61                          *

---
+++ nixos/home/hyprland/scripts/brightness-kbd.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Get keyboard brightness
+get_kbd_backlight() {
+	echo $(brightnessctl -d '*::kbd_backlight' -m | cut -d, -f4)
+}
+
+# Change brightness
+change_kbd_backlight() {
+	brightnessctl -d *::kbd_backlight set "$1"
+}
+
+# Execute accordingly
+case "$1" in
+	"--get")
+		get_kbd_backlight
+		;;
+	"--inc")
+		change_kbd_backlight "+30%"
+		;;
+	"--dec")
+		change_kbd_backlight "30%-"
+		;;
+	*)
+		get_kbd_backlight
+		;;
+esac
