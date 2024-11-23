b908cc3ac41279ed1e5d94b80bc9212eb94e4adc
ebonian <52095091+ebonian@users.noreply.github.com>
2024-11-23 18:45:24 +0700

    72 fix kbd-backlight script

--- nixos/home/hyprland/scripts/kbd-backlight.sh
+++ nixos/home/hyprland/scripts/kbd-backlight.sh
@@ -2,12 +2,46 @@
 
 # Get keyboard brightness
 get_kbd_backlight() {
-	echo $(brightnessctl -d 'asus::kbd_backlight' -m | cut -d, -f4)
+	brightnessctl -d 'asus::kbd_backlight' -m | cut -d, -f3
 }
 
 # Change brightness
-change_kbd_backlight() {
+set_kbd_backlight() {
 	brightnessctl -d asus::kbd_backlight set "$1"
+}
+
+# Clamp a value between a minimum and maximum
+clamp() {
+	local value="$1"
+	local min="$2"
+	local max="$3"
+	if (( value < min )); then
+		echo "$min"
+	elif (( value > max )); then
+		echo "$max"
+	else
+		echo "$value"
+	fi
+}
+
+# Increment or decrement brightness
+adjust_kbd_backlight() {
+	local direction="$1"
+	local current=$(get_kbd_backlight)
+	local new_value
+
+	case "$direction" in
+		"inc")
+			new_value=$((current + 1))
+			;;
+		"dec")
+			new_value=$((current - 1))
+			;;
+	esac
+
+	# Clamp the new value between 0 and 3
+	new_value=$(clamp "$new_value" 0 3)
+	set_kbd_backlight "$new_value"
 }
 
 # Execute accordingly
@@ -16,12 +50,12 @@ case "$1" in
 		get_kbd_backlight
 		;;
 	"--inc")
-		change_kbd_backlight "+30%"
+		adjust_kbd_backlight "inc"
 		;;
 	"--dec")
-		change_kbd_backlight "30%-"
+		adjust_kbd_backlight "dec"
 		;;
 	*)
-		get_kbd_backlight
+		echo "Usage: $0 --get | --inc | --dec"
 		;;
 esac
